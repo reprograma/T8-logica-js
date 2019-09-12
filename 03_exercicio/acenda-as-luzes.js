2969cb0bb860049b3d71e7580c2b8cbd29c437a2
Beatriz Rizental <beatriz.rizental@gmail.com>
2019-09-12 11:54:38 -0300

    exercicio 3: resolve criacao da tabela

--- 03_exercicio/acenda-as-luzes.js
+++ 03_exercicio/acenda-as-luzes.js
@@ -1,9 +1,24 @@
-function acendaAsLuzes(linha, coluna) {
-  let tabela = [ [ 0, 0, 0, 0, 0 ],
-                 [ 0, 0, 0, 0, 0 ],
-                 [ 0, 0, 0, 0, 0 ],
-                 [ 0, 0, 0, 0, 0 ],
-                 [ 0, 0, 0, 0, 0 ] ]
+function criaTabela(num_linhas, num_colunas) {
+  let tabela = [ ]
+  for (let i = 0; i < num_linhas; i = i + 1) {
+    let linha = [ ]
+    for (let j = 0; j < num_colunas; j = j + 1) {
+      linha.push(0)
+    }
+    tabela.push(linha)
+  }
+  return tabela
+}
+
+function acendaAsLuzes(tabela, linha, coluna) {
+  if (!(linha >= 0 && linha < tabela.length)) {
+    throw 'Opa, index da linha fora do que pode, ai ai ai.'
+  }
+
+  if (!(coluna >= 0 && coluna < tabela[0].length)) {
+    throw 'Opa, index da coluna fora do que pode, ai ai ai.'
+  }
+
   for (let i = 0; i < tabela[linha].length; i = i + 1) {
     tabela[linha][i] = 1
   }
